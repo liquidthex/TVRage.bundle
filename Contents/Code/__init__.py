adc0035d022ddc042e1d6bbc132dbe5272b05ce6
sander1 <sander@plexapp.com>
2013-01-31 16:09:08 +0100

    Set contributes_to to empty list (makes it no longer show up under TheTVDB Agent).

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,17 +1,17 @@
-#TVRage "better than nothing" poster grabber
-TVRAGE_SEARCH = 'http://services.tvrage.com/feeds/search.php?show=%s'
-
 def Start():
-  HTTP.CacheTime = CACHE_1DAY
+
+	HTTP.CacheTime = CACHE_1DAY
+
 
 class TVRageAgent(Agent.TV_Shows):
-  name = 'TVRage'
-  languages = [Locale.Language.English]
-  primary_provider = False
-  contributes_to = ['com.plexapp.agents.thetvdb']
 
-  def search(self, results, media, lang):
-    pass
+	name = 'TVRage'
+	languages = [Locale.Language.English]
+	primary_provider = False
+	contributes_to = []
 
-  def update(self, metadata, media, lang):
-    pass
+	def search(self, results, media, lang):
+		pass
+
+	def update(self, metadata, media, lang):
+		pass
